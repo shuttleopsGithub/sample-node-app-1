9fa7570f2118804d94973d7b9337949f09a9112c
David Found <davidf@indellient.com>
2019-10-31 13:34:45 -0400

    Run as root.

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -16,6 +16,7 @@ pkg_name=sample-node-app-1
 pkg_origin=your_origin
 pkg_version="1.0.0"
 pkg_deps=(core/node)
+pkg_svc_user=root
 
 # Habitat provides you with a number of built-in "callbacks" to use
 # in the course of your build, all of which are explained in the docs
